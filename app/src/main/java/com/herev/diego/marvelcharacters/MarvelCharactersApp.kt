57468bbc7f5d06556084c1caa4207c1c08eaea34
Diego Hernando <diego.herev@gmail.com>
2022-10-13 22:16:18 +0200

    Added Application class

---
+++ app/src/main/java/com/herev/diego/marvelcharacters/MarvelCharactersApp.kt
@@ -0,0 +1,8 @@
+package com.herev.diego.marvelcharacters
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MarvelCharactersApp : Application() {
+}
